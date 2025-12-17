918b6b121f1fcf0d2e5745b8d52728caf916636d
Milos Jaranovic <mj@kingcon.dk>
2025-12-18 00:35:06 +0100

    B2B example workspace first try.

--- examples/services/Publish-Services.ps1
+++ examples/services/Publish-Services.ps1
@@ -3,27 +3,27 @@ Write-Host "Publishing Services to SPAS Service Registry"
 spas-service publish --archive ./order-service-1.0.0.zip `
 --image-repository spas-examples/order-service `
 --image-tag 1.0.0 `
---image-digest sha256:5997af4858cefbb6944ac1f2ba78e66abd7d6b585dbe0ed80154a0df4f71eab5
+--image-digest sha256:a4cee00f0b7477f730ea9dad7f1404d2570d4ce99bb92ac853397b6e89e14b08
 
 Write-Host "Published Order Service"
 
 spas-service publish --archive ./inventory-service-1.0.0.zip `
 --image-repository spas-examples/inventory-service `
 --image-tag 1.0.0 `
---image-digest sha256:432416a457a4fba25c1f692f41cc40472fbd5b587bed4963c8d4bb11c93474e3
+--image-digest sha256:04837fe89384aea85eaeba0fa8ca00de620bf6e93777ae90797558d126d5fff4
 
 Write-Host "Published Inventory Service"
 
 spas-service publish --archive ./product-service-1.0.0.zip `
 --image-repository spas-examples/product-service `
 --image-tag 1.0.0 `
---image-digest sha256:af61b06f67c05f59ac1f91dc62fe601ee56281e974f0e6df7027fd7d777239f2
+--image-digest sha256:99502dbc88bdba6acffb175ea9b13ccd9fa2de7544c822a8e2479a8306078624
 
 Write-Host "Published Product Service"
 
 spas-service publish --archive ./subscription-service-1.0.0.zip `
 --image-repository spas-examples/subscription-service `
 --image-tag 1.0.0 `
---image-digest sha256:7f7416d9d6d6c4fd89109c53fdd04dca6028858059e7705ebc2d42a9bec84d53
+--image-digest sha256:7bab4e9cf2482f1dea73bb9698f1e96c1fb629ebc6d2ad50caf67a6a07643063
 
 Write-Host "Published Subscription Service"

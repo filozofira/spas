6d8117b8edcc4db86a16f5d1e2bbe9dbc550cc74
Milos Jaranovic <mj@kingcon.dk>
2025-12-17 22:34:49 +0100

    E2E Example: e2e worked on existing session.

--- examples/services/Publish-Services.ps1
+++ examples/services/Publish-Services.ps1
@@ -3,20 +3,20 @@ Write-Host "Publishing Services to SPAS Service Registry"
 spas-service publish --archive ./order-service-1.0.0.zip `
 --image-repository spas-examples/order-service `
 --image-tag 1.0.0 `
---image-digest sha256:fc6d849a1aa9862168c191ae10f30a7a5eecfd75fcdb591f9e53854c9bf19fcc
+--image-digest sha256:bab325ae683beacead69c21a845cac080be7b1f4447dffcd1125eb3ff6096cbb
 
 Write-Host "Published Order Service"
 
 spas-service publish --archive ./inventory-service-1.0.0.zip `
 --image-repository spas-examples/inventory-service `
 --image-tag 1.0.0 `
---image-digest sha256:c8264297160e092d1fbe9ed753a9563824aeea2b50b53d631ada65ec70da504c
+--image-digest sha256:4f6de5ba301c4da2570d659abca21189245e887abd539756063142007a6ce273
 
 Write-Host "Published Inventory Service"
 
 spas-service publish --archive ./product-service-1.0.0.zip `
 --image-repository spas-examples/product-service `
 --image-tag 1.0.0 `
---image-digest sha256:097ff2aea3eb7484fa06dc282d74a35c7556d401e0769284334c575eedf2f8f4
+--image-digest sha256:779b5e2393c0393db4a4f66eb84478d6ee467f669f14cfb875fc573c8a476e99
 
 Write-Host "Published Product Service"
